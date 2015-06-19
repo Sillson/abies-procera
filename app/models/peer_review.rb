a297693c0707a0146d0b1903f590d4b09405e6fa
Stuart Illson <stuartillson@Illson-W80471U6AGX.local>
2015-06-18 21:20:39 -0700

    scorecard added

--- app/models/peer_review.rb
+++ app/models/peer_review.rb
@@ -1,3 +1,8 @@
 class PeerReview < ActiveRecord::Base
 	belongs_to :user
+
+	def pr_mean
+		PeerReview.average(:loc_count).to_f
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,30 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
   has_many :peer_reviews
 
-  def sum_user_locs(builder_id)
-  	PeerReview.where(user_id: builder_id).sum(:loc_count)
+  def sum_user_locs
+  	PeerReview.where(user_id: self.id).sum(:loc_count)
+  end
+
+  def ind_mean_difference(pr_mean)
+  	self.sum_user_locs - pr_mean
+  end
+
+  def calculate_user_score(pr_mean)
+  	ind_mean_diff = self.ind_mean_difference(pr_mean)
+  	if ind_mean_diff <= 0
+  		if ind_mean_diff.abs >= pr_mean/2
+  			return 1
+  		else
+  			return 2
+  		end
+  	else
+  		if ind_mean_diff.abs <= pr_mean/2 
+  			return 3
+  		else
+  			return 4
+  		end
+  	end
   end
 end
